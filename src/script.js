5216605b10df415c30769e4643083b44ce340d85
James Madden <jamesbmadden@gmail.com>
2021-05-21 16:27:16 -0700

    Updates

--- src/script.js
+++ src/script.js
@@ -1,77 +1,74 @@
-import { LitElement, html } from '../_snowpack/pkg/lit.js';
+import { LitElement, html, css } from '../_snowpack/pkg/lit.js';
 import projects from './data/projects.js';
 import './components/image-card/image-card.js';
 
-window.addEventListener('wheel', event => {
-  event.stopPropagation();
-});
+export default class Root extends LitElement {
 
-class Root extends LitElement {
+  static get styles () {
+    return css`
+    .titles {
+      mix-blend-mode: difference;
+      color: white;
+      position: absolute;
+      top: 50%; left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 3;
+    }
+    .titles h1 { font-size: 4rem; margin: 0; font-style: italic; }
+    .titles h2 { font-size: 2rem; margin: 0; font-weight: 600; }
+
+    .offset-content {
+      position: relative;
+      width: 100%;
+      height: calc(100vh - 128px);
+    }
+    
+    .section-header {
+      font-family: 'Lato', sans-serif;
+      box-sizing: border-box;
+      font-size: 3rem;
+      color: white;
+      mix-blend-mode: difference;
+      margin: 0;
+      margin-bottom: 1rem;
+      position: relative;
+      padding-left: 3rem;
+      font-weight: 600;
+      z-index: 2;
+    }
+    .section-header::before {
+      content: "";
+      position: absolute;
+      top: 1rem;
+      left: 1rem;
+      background: white;
+      width: 1rem; height: 1rem;
+      border-radius: 50%;
+    }
+    .section-header::after {
+      content: "";
+      position: absolute;
+      top: 1rem;
+      background: white;
+      width: 4rem; height: 1rem;
+      margin-left: 1rem; 
+      border-radius: 0.5rem;
+    }
+    .projects-grid {
+      box-sizing: border-box;
+      padding: 0 1.5rem;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 1.5rem;
+    }
+    img-card {
+      position: relative;
+    }
+    `;
+  }
 
   render () {
     return html`
-      <style>
-        * {
-          z-index: 2;
-        }
-        .titles {
-          mix-blend-mode: difference;
-          color: white;
-          position: absolute;
-          top: 50%; left: 50%;
-          transform: translate(-50%, -50%);
-          z-index: 3;
-        }
-        .titles h1 { font-size: 4rem; margin: 0; font-style: italic; }
-        .titles h2 { font-size: 2rem; margin: 0; font-weight: 600; }
-
-        .offset-content {
-          position: relative;
-          width: 100%;
-          height: calc(100vh - 128px);
-        }
-        
-        .section-header {
-          font-family: 'Lato', sans-serif;
-          box-sizing: border-box;
-          font-size: 3rem;
-          color: white;
-          mix-blend-mode: difference;
-          margin: 0;
-          margin-bottom: 1rem;
-          position: relative;
-          padding-left: 3rem;
-          font-weight: 600;
-        }
-        .section-header::before {
-          content: "";
-          position: absolute;
-          top: 1rem;
-          left: 1rem;
-          background: white;
-          width: 1rem; height: 1rem;
-          border-radius: 50%;
-        }
-        .section-header::after {
-          content: "";
-          position: absolute;
-          top: 1rem;
-          background: white;
-          width: 4rem; height: 1rem;
-          margin-left: 1rem; 
-          border-radius: 0.5rem;
-        }
-        .projects-grid {
-          box-sizing: border-box;
-          padding: 0 1.5rem;
-          display: grid;
-          grid-template-columns: repeat(4, 1fr);
-          grid-gap: 1.5rem;
-        }
-        img-card {
-          position: relative;
-        }
-      </style>
       <div class="titles">
         <h1>James Madden</h1>
         <h2>Developer, designer.</h2>
