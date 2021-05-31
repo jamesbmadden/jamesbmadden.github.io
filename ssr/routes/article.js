d39ed172f5123075af5dec63e65300daf91b9559
James Madden <jamesbmadden@gmail.com>
2021-05-31 13:34:27 -0700

    read markdown file for post

--- ssr/routes/article.js
+++ ssr/routes/article.js
@@ -1,6 +1,9 @@
 import { html } from 'lit-html';
 import Article from '../../web/build/src/components/jbm-article/jbm-article.js';
 
+import fs from 'fs';
+
 export default function article(data) {
-  return html([`<jbm-article title=${data.title} article=${data.bio} tags=${data.tags}></jbm-article>`]);
+  const postData = fs.readFileSync("./web/build" + data.post).toString();
+  return html([`<jbm-article img="${data.img}" title="${data.title}" article="${postData}" tags="${data.tags}" animate=${false}></jbm-article>`]);
 }
