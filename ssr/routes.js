576dfc1dd01e1f4365517637bb33b57f1f0faa4b
James Madden <jamesbmadden@gmail.com>
2021-05-31 12:52:33 -0700

    Add 404 page

--- ssr/routes.js
+++ ssr/routes.js
@@ -1,8 +1,13 @@
 import IndexTemplate from './routes/index.js';
+import Template404 from './routes/404.js';
 
 export default [
   {
     path: "index.html",
     template: IndexTemplate
+  },
+  {
+    path: "404.html",
+    template: Template404
   }
 ];
---
+++ ssr/routes/404.js
@@ -0,0 +1,20 @@
+import { html } from 'lit';
+
+export default html`
+  <style>
+    h1 {
+      position: absolute;
+      bottom: 2rem; left: 0.5rem;
+      font-size: 3rem;
+      margin: 0;
+    }
+    h2 {
+      position: absolute;
+      bottom: 0.5rem; left: 0.5rem;
+      font-size: 1.5rem;
+      margin: 0;
+    }
+  </style>
+  <h1>404 😐</h1>
+  <h2>Watch the pretty background 😌</h2>
+`;
