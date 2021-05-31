743014a9f383c0787c067811bd8e7141b4522304
James Madden <jamesbmadden@gmail.com>
2021-05-31 13:27:13 -0700

    Make all URLS relative to /

--- ssr/boilerplate.js
+++ ssr/boilerplate.js
@@ -5,6 +5,7 @@ export default function withBoilerplate (component) {
     <!DOCTYPE html>
       <html>
         <head>
+          <base href="/">
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://fonts.gstatic.com">
